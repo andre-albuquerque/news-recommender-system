057d57ff68088649a0975aec9c12b8e378fea367
André Albuquerque <andrealbuquerqueleo@gmail.com>
2021-08-09 15:26:36 -0300

    commit on 'main'

--- news_streamlit.py
+++ news_streamlit.py
@@ -25,7 +25,7 @@ st.write("___")
 
 @st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
 def init_connection():
-    return mysql.connector.connect("DATABASE_URL")
+    return mysql.connector.connect(**"DATABASE_URL")
 
 
 
